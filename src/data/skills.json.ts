46b2a91bebe968c6b956ad702468d54fe4228305
carissaf <carissa.fabellina@gmail.com>
2024-09-27 23:31:45 +0700

    Added Sections for Home

---
+++ src/data/skills.json.ts
@@ -0,0 +1,16 @@
+const skillsData = [
+    {
+        title: "Web Development",
+        techs: ["React", "Laravel", "Bootstrap", "Tailwind CSS"]
+    },
+    {
+        title: "UI/UX Design",
+        techs: ["Figma", "Adobe Illustrator", "Axure"]
+    },
+    {
+        title: "Data Science",
+        techs: ["Machine Learning", "Python", "SQL"]
+    }
+]
+
+export default skillsData;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,8 @@ export default {
 			'yellow': '#FBDD7F',
 			'dark-blue': '#1C325B',
 			'blue': '#327B94',
-			'light-blue': '#7FB7D0'
+			'light-blue': '#7FB7D0',
+			'white': '#FFFFFF',
 		}
 	},
 	plugins: [],
