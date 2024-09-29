df8636aba8d42d4013b94bc5bc3cf318980b9da3
carissaf <carissa.fabellina@gmail.com>
2024-09-29 20:07:46 +0700

    Added Scroll Jacking Home Page

---
+++ src/utils/scroll-jacking.ts
@@ -0,0 +1,48 @@
+const sections: NodeListOf<Element> = document.querySelectorAll(".section-scroll");
+var currentSectionIndex: number = 0;
+var isThrottled: boolean = false;
+
+document.addEventListener("wheel", (event: WheelEvent): void =>{
+    event.preventDefault();
+
+    if(isThrottled) return;
+
+    const delta = Math.sign(event.deltaY); //sign returns -1 if number is negative and 1 if number is positive
+    if(delta > 0){ //scroll down
+        goToNextSection();
+    }else if(delta < 0){ //scroll up
+        goToPrevSection();
+    }
+
+    throttleScroll()
+},{ passive: false })
+
+const throttleScroll = ()=>{
+    console.log("throtltltlelelel")
+    isThrottled = true;
+    setTimeout(()=>{
+        isThrottled = false;
+    }, 1000)
+}
+
+const goToNextSection = () => {
+    if(currentSectionIndex < sections.length-1) {
+        currentSectionIndex++;
+        scrollToSection(currentSectionIndex);
+    }
+}
+
+const goToPrevSection = () => {
+    if(currentSectionIndex > 0) {
+        currentSectionIndex--;
+        scrollToSection(currentSectionIndex);
+    }
+}
+
+const scrollToSection = (currentSectionIndex: number) => {
+    console.log(currentSectionIndex);
+    sections[currentSectionIndex].scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    })
+}
